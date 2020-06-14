5c059bd851ec445d72509f4f955c589aa15ca2a0
piyushthummar <piyushthummar305@gmail.com>
2020-06-14 18:39:14 -0400

    Both files created but code is not working...

--- src/calling.erl
+++ src/calling.erl
@@ -10,4 +10,30 @@
 -author("PIYUSH").
 
 %% API
--export([]).
+-export([messagePassing/3]).
+messagePassing(Sender, Receivers, Master_Process_Id) ->
+  iterateList(Sender, Receivers, self()),
+  caller(Master_Process_Id, Sender).
+
+iterateList(Sender, Receivers, Process_ID) ->
+  lists:foreach(fun(Record) ->
+    register(Sender, Process_ID),
+    {_,_,MicroSecond} = erlang:now(),
+    Process_ID ! {request, Sender, Record, Process_ID, MicroSecond}
+                end, Receivers).
+
+caller(Master_Process_Id, Sender_Name) ->
+  receive
+    {request, Sender, Current_Receiver, Process_ID, MicroSecond} ->
+      timer:sleep(100),
+      Master_Process_Id ! {intro_Message,Sender,Current_Receiver,MicroSecond},
+      Process_ID ! {response, Master_Process_Id, Sender, Current_Receiver, MicroSecond};
+
+    {response,Master_Process_Id, Sender, Receiver, Timestamp} ->
+      timer:sleep(100),
+      Master_Process_Id ! {reply_Message, Sender, Receiver, Timestamp}
+
+  after
+    5000 ->
+      io:format("Process ~w has received no calls for 5 seconds, ending...", [Sender_Name])
+  end, caller(Master_Process_Id, Sender_Name).
--- src/exchange.erl
+++ src/exchange.erl
@@ -12,23 +12,26 @@
 %% API
 -export([start/0]).
 start() ->
-%%                io:fwrite("Hi this is piyush\n"),
-%%                CallsData = file:consult("src/calls.txt"),
-%%                Calling_List = element(2, CallsData),
-
 %%              (Reference: https://sites.google.com/site/gettingalongwitherlang/home/file-i-o/file-consult)
                 {Data, CallsData} = file:consult("src/calls.txt"),
                 io:format("~n ** Calls to be made ** ~n",[]),
-%%                io:fwrite("~s~n", [Data]),
+                Master_Process_id = self(),
+                List_of_Data = maps:from_list(CallsData),
+                [io:format("~w: ~w ~n",[Sender,List_of_Receivers]) || {Sender, List_of_Receivers} <- CallsData],
+                maps:fold(fun(Key, Value, ok) ->
+                  Process_ID = spawn(calling, messagePassing, [Key,Value,Master_Process_id])
+                          end,ok, List_of_Data),
+            print_Conversation_Message(Master_Process_id). %% 0
 
-%%                io:fwrite("~s~n",[CallsData]),
-%%
-%%              (Reference : https://stackoverflow.com/questions/16111542/understanding-and-using-foreach-in-erlang)
-                lists:foreach(fun(Tuple) ->
-                                            {Sender,Receiver_List} = Tuple,
-                                            io:format("~w: ~w ~n",[Sender,Receiver_List])
-
-                              end,
-                              CallsData),
-
-                io:fwrite("~n",[]).
+%% Print Initial and Reply message...
+print_Conversation_Message(Master_Process_id) ->
+  receive
+    {intro_Message, Sender, Receiver, Timestamp} ->
+      io:fwrite("~r received intro message from ~s [~t] ~n",[Sender, Receiver, Timestamp]),
+      print_Conversation_Message(Master_Process_id); %%Counter+1
+    {reply_Message, Sender, Receiver, Timestamp} ->
+      io:fwrite("~r received reply message from ~s [~t] ~n",[Sender, Receiver, Timestamp]),
+      print_Conversation_Message(Master_Process_id) %%Counter+1
+  after
+    10000 -> io:fwrite("Master has received no replies for 10 seconds, ending...~n",[])
+  end.
